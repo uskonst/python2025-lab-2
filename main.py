3cf8034133f3634ec337e14c600d9d638f228022
Konstantin Vyatkin <konst.vyat@yandex.ru>
2025-12-15 20:26:25 +0300

    deleted:    Coffe_sales.csv 	modified:   bibliography.txt 	new file:   currency.xml 	deleted:    lesson.py 	modified:   main.py 	deleted:    main2.py

--- main.py
+++ main.py
@@ -1,56 +1,48 @@
-# f = open("Coffee_sales.csv")
-
-# f.close
-
 import csv
-import statistics
+import random
+import xml.etree.ElementTree as ET
 
-def rline(reader):
-    fields = next(reader)
-    print(" | ".join(fields))
-    return fields
+AUTHOR_KEY = 'Автор'
+DATE_KEY = 'Дата поступления'
+TITLE_KEY = 'Название'
 
-def slice(coffee, start, end):
-    coffee.seek(0)
-    r = csv.reader(coffee)
-    for i in range(end):
-        if i >= start:
-            rline(r)
-        else:
-            next(r)
-    
-def findpop(coffee):
-    coffee.seek(0)
-    r = csv.DictReader(coffee)
-    
-    # res = statisrics.mode("")
-    
-    drinks = []
-    for i in r:
-        drinks.append(i["coffee_name"])
-    # print(drinks)
-    count = {d:0 for d in drinks}
-    
-    for d in drinks:
-        count[d] += 1
-    print(max(count, key=lambda k: count[k]))
-        
-# def month(coffee):
-#     months = input("1-12 ")
-#     coffee.seek(0)
-#     r = csv.DictReader(coffee)
-#     c = 0
-#     for i in r:
-#         if i["Monthsort"] == str(months):
-#             c += float(i["money"])
-#     print(c)
-#     return c
+def get_year(date):
+    return int(date.split('.')[-1].split()[0])
 
-if __name__ == "__main__":
-    with open("Coffe_sales.csv") as coffee: # Лучше использовать этот метод открытия файла
-        # slice(coffee, 8, 15)
-        findpop(coffee)
-        # month(coffee)
-        
-        
-    
+with open('books.csv', encoding='cp1251') as file:
+    rows = list(csv.DictReader(file, delimiter=';'))
+
+# Задание 1
+count = sum(len(row[TITLE_KEY]) > 30 for row in rows)
+print('Задание 1:', count)
+
+# Задание 2
+author = input("Автор:")
+filtered_books = []
+for row in rows:
+    year = get_year(row[DATE_KEY])      
+    if author in row[AUTHOR_KEY] and year in {2014, 2016, 2017}:
+        filtered_books.append(row)
+
+print('Задание 2:')
+for book in filtered_books:
+    print(book[AUTHOR_KEY], book[TITLE_KEY], get_year(book[DATE_KEY]))
+
+# Задание 3
+bibliography = random.sample(rows, 20)
+
+with open('bibliography.txt', 'w', encoding='utf-8') as file:
+    for index, book in enumerate(bibliography, start=1):
+        file.write(f"{index}. {book[AUTHOR_KEY]}. {book[TITLE_KEY]} - {get_year(book[DATE_KEY])}\n")
+print('Задание 3: Файл завершен')
+
+# Задание 4
+tree = ET.parse('currency.xml')
+root = tree.getroot()
+
+print('Задание 4:')
+for valute in root.findall('Valute'):
+    Nominal = valute.find('Nominal').text
+    if Nominal == 1:
+        Name = valute.find('Name').text
+        print(Name)
